46464ab572ea704b916c9415cbef7227d85178be
Jatin Bhatt <jatin.b.php@gmail.com>
2021-11-23 18:19:20 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent 
+{
+  public appPages = [
+    { title: 'Manqabat', url: '/home', icon: 'home', categories: []},//[0]
+    { title: 'Marsiya', url: '/shop', icon: 'search', categories: []},//[1]
+    { title: 'Nauha', url: '/cart', icon: 'bag', categories: []},//[2]
+    { title: 'Offline', url: '/cart', icon: 'bag', categories: []},//[3]
+    { title: 'About', url: '/cart', icon: 'bag', categories: []},//[4]
+    { title: 'Profile', url: '/cart', icon: 'bag', categories: []},//[5]
+    { title: 'Settings', url: '/cart', icon: 'bag', categories: []},//[6]
+  ];
+  constructor()
+  {}
+}
